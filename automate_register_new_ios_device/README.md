a2f7605a04651ca9d0667866496f215cbd78a4cc
Levi Bostian <levi@customer.io>
2023-03-31 13:15:58 -0500

    begin settting up firebaes to trigger GH action

---
+++ automate_register_new_ios_device/README.md
@@ -0,0 +1,4 @@
+# Automate registering new iOS devices to company Apple Developer account 
+
+TODO give flow for what needs to happen to get a new tester added to an iOS build 
+
---
+++ automate_register_new_ios_device/src/index.js
@@ -0,0 +1,40 @@
+const {
+  onNewTesterIosDevicePublished,
+} = require("firebase-functions/v2/alerts/appDistribution");
+const http = require("node-fetch");
+
+// This function gets called automatically by Firebase when a new test device is added to App Distribution for this Firebase Project. 
+// To automate adding this new iOS device to our company's Apple Developer account, we will trigger the CI server for this GitHub repository which is setup 
+// to automate registering new devices, building iOS apps, and re-create code signing. 
+exports.registerNewDeviceAdded = onNewTesterIosDevicePublished(async (event) => {
+  const appId = event.appId;
+  const {
+    testerDeviceIdentifier,
+    testerDeviceModelName,
+    testerEmail,
+    testerName,
+  } = event.data.payload;
+  const githubApiKeyToTriggerCIServer = process.env.GITHUB_API_KEY_TRIGGER_CI_SERVER_REGISTER_NEW_TOKEN;
+
+  if (!githubApiKeyToTriggerCIServer) {
+    throw new Error(`Forgot to set GITHUB_API_KEY_TRIGGER_CI_SERVER_REGISTER_NEW_TOKEN environment variable to authenticate with GitHub to run CI server`);
+  }
+  
+  // https://docs.github.com/en/rest/actions/workflows?apiVersion=2022-11-28#create-a-workflow-dispatch-event
+  await http("https://api.github.com/repos/customerio/apple-code-signing/actions/workflows/register-new-device.yml/dispatches", {
+    method: "POST",
+    headers: {
+      Accept: "application/vnd.github+json",
+      Authorization: `Bearer ${githubApiKeyToTriggerCIServer}`
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+    body: JSON.stringify({
+      ref: "main", // name of the branch where the workflow file exists that we want to run. 
+      inputs: {
+        ios_device_udid: testerDeviceIdentifier,
+        device_model_name: testerDeviceModelName,
+        device_tester_email: testerEmail
+      }
+    })
+  });
+};
